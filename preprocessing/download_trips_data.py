fe101dc8854cdbb045799282865eb47adf569721
Edoardo Scarpel <edoardo.scarpel@gmail.com>
2025-01-16 10:38:09 +0100

    Bug fix

--- preprocessing/download_trips_data.py
+++ preprocessing/download_trips_data.py
@@ -3,6 +3,7 @@ import platform
 import requests
 import zipfile
 from urllib.parse import urlparse
+from tqdm import tqdm
 
 data_path = "../data/"
 if platform.system() == "Linux":
@@ -10,7 +11,7 @@ if platform.system() == "Linux":
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-def download_and_extract(url, target_directory):
+def download_and_extract(url, target_directory, tbar = None):
     """
     Downloads a file from the given URL, saves it to the specified directory,
     extracts it if it's a ZIP file, and removes the ZIP file after extraction.
@@ -27,25 +28,32 @@ def download_and_extract(url, target_directory):
         save_path = os.path.join(target_directory, filename)
 
         # Download the file
-        print(f"Downloading file from {url}...")
+        if tbar is not None:
+            tbar.set_description(f"Downloading {filename}")
+        else:
+            print(f"Downloading file from {url}...")
         response = requests.get(url, stream=True)
         response.raise_for_status()  # Raise an exception for HTTP errors
 
         with open(save_path, "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
-        print(f"File downloaded successfully and saved to {save_path}")
 
         # Check if the file is a ZIP file and extract it
         if zipfile.is_zipfile(save_path):
-            print(f"Extracting contents of {save_path} to {target_directory}...")
+            if tbar is not None:
+                tbar.set_description(f"Extracting {filename}")
+            else:
+                print(f"Extracting contents of {save_path} to {target_directory}...")
             with zipfile.ZipFile(save_path, "r") as zip_ref:
                 zip_ref.extractall(target_directory)
-            print(f"Extraction complete. Files extracted to {target_directory}")
 
             # Remove the ZIP file
             os.remove(save_path)
-            print(f"Removed the ZIP file: {save_path}")
+            if tbar is not None:
+                tbar.set_description(f"Removed the ZIP file: {save_path}")
+            else:
+                print(f"Removed the ZIP file: {save_path}")
         else:
             print(f"The file is not a ZIP archive. No extraction performed.")
 
@@ -64,9 +72,11 @@ def main():
         os.makedirs(save_path)
         print(f"Directory '{save_path}' created.")
 
+    tbar = tqdm(range(12), desc='Downloading files', position=0, leave=True)
+
     for month in range(0, 12):
         url = 'https://s3.amazonaws.com/hubway-data/2022' + str(month+1).zfill(2) + '-bluebikes-tripdata.zip'
-        download_and_extract(url, save_path)
+        download_and_extract(url, save_path, tbar)
 
     os.remove(save_path + '__MACOSX/')  # Remove the __MACOSX directory
 
